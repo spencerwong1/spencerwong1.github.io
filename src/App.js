e57fa7ca7e024a12eb871bf4318b896ef5e1c813
spencerwong1 <100222155+spencerwong1@users.noreply.github.com>
2025-05-23 03:04:41 +1000

    basic website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Personal website currently under construction </h1>
+      <a href="https://www.linkedin.com/in/spencerwongg/"> LinkedIn </a>
+      <div class = "empty"style={{height:"10vh"}}></div>
+      <a href="https://github.com/spencerwong1/"> Github </a>
+      <div class = "empty"style={{height:"10vh"}}></div>
+      <a href="https://spencerwong1.github.io/dropfinder/"> DropFinder </a>
+     <div class = "empty"style={{height:"10vh"}}></div>
+      <a href="https://spencerwong1.github.io/ezmail/"> EZMAIL </a>
     </div>
   );
 }
+
 
 export default App;
